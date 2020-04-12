d1aef58bf9e299fc7c012c1ae2cb7bf871aea479
nguyenbac <nguyenbac5299@gmail.com>
2020-04-12 20:00:12 +0700

    commit1

--- app/src/main/java/com/example/assignmentt6/MainActivity.java
+++ app/src/main/java/com/example/assignmentt6/MainActivity.java
@@ -18,17 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         listView=findViewById(R.id.list);
 
-        list.add(new ContactModel("A","Chúc mừng bạn đã trúng 1 tỷ đồng","9 th4",false));
-        list.add(new ContactModel("B","Chúc mừng bạn đã trúng 1 tỷ đồng","9 th4",false));
-        list.add(new ContactModel("C","Chúc mừng bạn đã trúng 1 tỷ đồng","9 th4",false));
-        list.add(new ContactModel("D","Chúc mừng bạn đã trúng 1 tỷ đồng","9 th4",false));
-        list.add(new ContactModel("E","Chúc mừng bạn đã trúng 1 tỷ đồng","9 th4",false));
-        list.add(new ContactModel("F","Chúc mừng bạn đã trúng 1 tỷ đồng","9 th4",false));
-        list.add(new ContactModel("G","Chúc mừng bạn đã trúng 1 tỷ đồng","9 th4",false));
-        list.add(new ContactModel("H","Chúc mừng bạn đã trúng 1 tỷ đồng","9 th4",false));
-        list.add(new ContactModel("K","Chúc mừng bạn đã trúng 1 tỷ đồng","9 th4",false));
-        list.add(new ContactModel("Q","Chúc mừng bạn đã trúng 1 tỷ đồng","9 th4",false));
-        list.add(new ContactModel("R","Chúc mừng bạn đã trúng 1 tỷ đồng","9 th4",false));
+        list.add(new ContactModel("A","Chúc mừng bạn đã trúng 1 tỷ đồng, vui lòng liên hệ chúng tôi để nhận thưởng","9 th4",false));
+        list.add(new ContactModel("B","Chúc mừng bạn đã trúng 1 tỷ đồng, vui lòng liên hệ chúng tôi để nhận thưởng","9 th4",false));
+        list.add(new ContactModel("C","Chúc mừng bạn đã trúng 1 tỷ đồng, vui lòng liên hệ chúng tôi để nhận thưởng","9 th4",false));
+        list.add(new ContactModel("D","Chúc mừng bạn đã trúng 1 tỷ đồng, vui lòng liên hệ chúng tôi để nhận thưởng","9 th4",false));
+        list.add(new ContactModel("E","Chúc mừng bạn đã trúng 1 tỷ đồng, vui lòng liên hệ chúng tôi để nhận thưởng","9 th4",false));
+        list.add(new ContactModel("F","Chúc mừng bạn đã trúng 1 tỷ đồng, vui lòng liên hệ chúng tôi để nhận thưởng","9 th4",false));
+        list.add(new ContactModel("G","Chúc mừng bạn đã trúng 1 tỷ đồng, vui lòng liên hệ chúng tôi để nhận thưởng","9 th4",false));
+        list.add(new ContactModel("H","Chúc mừng bạn đã trúng 1 tỷ đồng, vui lòng liên hệ chúng tôi để nhận thưởng","9 th4",false));
+        list.add(new ContactModel("K","Chúc mừng bạn đã trúng 1 tỷ đồng, vui lòng liên hệ chúng tôi để nhận thưởng","9 th4",false));
+        list.add(new ContactModel("Q","Chúc mừng bạn đã trúng 1 tỷ đồng, vui lòng liên hệ chúng tôi để nhận thưởng","9 th4",false));
+        list.add(new ContactModel("R","Chúc mừng bạn đã trúng 1 tỷ đồng, vui lòng liên hệ chúng tôi để nhận thưởng","9 th4",false));
 
         ContactAapter adapter=new ContactAapter(list);
         listView.setAdapter(adapter);
